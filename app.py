6c111b3dbd40f62603f3fd7af41fabfd6066bf12
xuheng <945806876@qq.com>
2020-05-25 18:39:10 +0800

    edit 0525

--- app.py
+++ app.py
@@ -16,6 +16,10 @@ db = SQLAlchemy(app)
 
 # 表单的类
 class Dhfs(db.Model):
+    """
+    电话费表
+    """
+    __tablename__ = 'dhfs'
     sd = db.Column(db.Integer, primary_key=True)
     tr = db.Column(db.Float())
     dx = db.Column(db.Float())
@@ -23,9 +27,20 @@ class Dhfs(db.Model):
     yx = db.Column(db.Float())
     hj = db.Column(db.Float())
 
+    def get_columns(self):
+        return [col.name for col in self.__table__.columns]
+
+    def to_dict(self):
+        return dict([(col, getattr(self, col)) for col in self.get_columns()])
+
+
     def __repr__(self):
         return '{0},{1},{2},{3},{4},{5}'.format(self.sd, self.tr, self.dx, self.kt, self.yx, self.hj)
 
+
+A = Dhfs().to_dict()
+
+print(A)
 
 
 # 建表
@@ -37,7 +52,7 @@ class Dhfs(db.Model):
 
 class DhfsDao():
     # 新增记录
-    def create_rd(self, rd_sd, rd_tr, rd_dx, rd_kt, rd_yx, rd_hj):
+    def create_rd(self, rd_sd, rd_tr,rd_dx, rd_kt, rd_yx, rd_hj):
         new_rd = Dhfs(sd=rd_sd, tr=rd_tr, dx=rd_dx, kt=rd_kt, yx=rd_yx, hj=rd_hj)
         db.session.add(new_rd)
         db.session.commit()
@@ -54,10 +69,10 @@ class DhfsDao():
         modified_rd = Dhfs.query.get(rd.sd)
         db.session.commit()
         return modified_rd
- 
+
     # 显示表
     def list_all(self):
-        return Dhfs.query.order_by(Dhfs.sd).all()
+        return Dhfs.query.order_by().all()
 
     # 查询记录
     def get_rd(self, sd):
@@ -65,7 +80,7 @@ class DhfsDao():
 
 # Form单类
 
-class NewDhfForm(Form):
+class NEDhfForm(Form):
     sd = IntegerField('费用账期:')
     tr = FloatField('天润费用:')
     dx = FloatField('东信费用:')
@@ -73,17 +88,6 @@ class NewDhfForm(Form):
     yx = FloatField('颐信费用:')
     hj = FloatField('总计费用:')
     submit = SubmitField('提交:')
-
-class EditNoteForm(Form):
-    sd = IntegerField('费用账期:')
-    tr = FloatField('天润费用:')
-    dx = FloatField('东信费用:')
-    kt = FloatField('科天费用:')
-    yx = FloatField('颐信费用:')
-    hj = FloatField('总计费用:')
-    submit = SubmitField('提交:')
-
-
 
 # 网页路由
 @app.route('/')
@@ -96,37 +100,26 @@ def index():
 
 @app.route('/new', methods=['GET', 'POST'])
 def new_rd():    
-    form = NewDhfForm()
+    form = NEDhfForm()
 
     if request.method == 'POST':
-        sd = request.form['sd']
-        tr = request.form['tr']
-        dx = request.form['dx']
-        kt = request.form['kt']
-        yx = request.form['yx']
-        hj = request.form['hj']
         dservice = DhfsDao()
-        dservice.create_rd(sd, tr, dx, kt, yx, hj)
+        dservice.create_rd(request.form['sd'],request.form['tr'], request.form['dx'], request.form['kt'], request.form['yx'], request.form['hj'])
         return redirect(url_for('index'))
 
-    return render_template('new_dhfrd.html', form=form)
+    return render_template('new_dhfrd.html', form=form, tls= "新增记录")
 
 
 @app.route('/edit/<int:rd_sd>', methods=['GET','POST'])
 def edit_rd(rd_sd):
-    form = EditNoteForm()
+    form = NEDhfForm()
     rd = DhfsDao().get_rd(rd_sd)
     if request.method == 'POST':
-        tr = request.form['tr']
-        dx = request.form['dx']
-        kt = request.form['kt']
-        yx = request.form['yx']
-        hj = request.form['hj']
-        rd.tr = tr
-        rd.dx = dx
-        rd.kt = kt
-        rd.yx = yx
-        rd.hj = hj
+        rd.tr = request.form['tr']
+        rd.dx = request.form['dx']
+        rd.kt= request.form['kt']
+        rd.yx= request.form['yx']
+        rd.hj= request.form['hj']
         DhfsDao().update_rd(rd)
         return redirect(url_for('index'))        
     form.sd.data = rd.sd
@@ -135,7 +128,7 @@ def edit_rd(rd_sd):
     form.kt.data = rd.kt
     form.yx.data = rd.yx
     form.hj.data = rd.hj
-    return render_template('edit_rd.html', form=form)
+    return render_template('new_dhfrd.html', form=form, tls = "修改记录")
 
 
 @app.route('/delete/<int:rd_sd>', methods=['GET'])
@@ -149,8 +142,3 @@ def delete_rd(rd_sd):
 @app.route('/loading')
 def load1():
     return '此页面,正在开发中.......'
-
-    
-
-
-
